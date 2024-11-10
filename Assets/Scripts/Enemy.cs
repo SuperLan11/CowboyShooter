39047ee20bec4c23a2e242e73abf924916b102af
SuperLan11 <92647630+SuperLan11@users.noreply.github.com>
2024-11-10 18:13:45 -0500

    updated enemy code and prevented camera jarring issue in player script

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -13,17 +13,17 @@ using UnityEngine.Scripting.APIUpdating;
 using Vector3 = UnityEngine.Vector3;
 
 public class Enemy : Character
-{    
+{
     private NavMeshAgent agent;
-    
+
     // make these Transform since Vector3 can't be dragged in inspector
     [SerializeField] private Transform destination1;
     [SerializeField] private Transform destination2;
-    private List<Transform> destList = new List<Transform>();
+    private List<Vector3> destList = new List<Vector3>();
 
-    private bool switchingDest;
-    private float destCooldown;
-    private float maxDestCooldown;    
+    [SerializeField] public float destCooldown;
+    [SerializeField] private float maxDestCooldown;
+    [SerializeField] private bool switchingDest;
 
     [SerializeField] private GameObject player;
     [SerializeField] private float sightRange;
@@ -31,108 +31,87 @@ public class Enemy : Character
     private bool playerSighted;
 
     void Start()
-    {        
+    {
         agent = GetComponent<NavMeshAgent>();
         player = FindObjectOfType<Player>().gameObject;
         playerNear = false;
-        playerSighted = false;        
-        
-        destList.Add(destination1);
-        destList.Add(destination2);
+        playerSighted = false;
+        sightRange = 5f;
+
+        destList.Add(destination1.position);
+        destList.Add(destination2.position);
         agent.destination = destination1.position;
-
-        // work on offmeshlinks
-
-        // maxDestCooldown changes how frequently NavMesh can change destinations in seconds
-        // this also prevents navmesh from assigning the same destination over and over       
-        destCooldown = 0f;        
-        maxDestCooldown = 0.5f;        
+        // in seconds
+        destCooldown = 0f;
+        maxDestCooldown = 0.5f;
         switchingDest = false;
 
-        // in seconds
         shootCooldown = 0f;
         maxShootCooldown = 1f;
-
         // this is only here to give feedback for shooting
         shootSfx = GetComponent<AudioSource>();
     }
-    
+
     protected override void Shoot(GameObject player)
-    {        
+    {
         player.GetComponent<Player>().TakeDamage(1);
-        if(shootSfx != null)
-            shootSfx.Play();
-    }            
+        shootSfx.Play();
+    }
 
     public bool PlayerSighted(Vector3 enemyPos)
     {
-        Ray ray = Camera.main.ScreenPointToRay(enemyPos);
         RaycastHit hit;
-        
         Vector3 direction = (Camera.main.transform.position - transform.position).normalized;
         // draw a raycast from enemy to player to see if player is sighted
-        if(Physics.Raycast(transform.position, direction, out hit, Mathf.Infinity))
+        if (Physics.Raycast(transform.position, direction, out hit, Mathf.Infinity))
         {
-            //Debug.Log("hit.name: " + hit.transform.gameObject.name);
-            if (hit.transform.gameObject.name == "Player")                            
-                return true;            
-        }                    
+            //Debug.Log("hit.name: " + hit.transform.gameObject.name);            
+            if (hit.transform.gameObject.name == "Player")
+                return true;
+        }
         return false;
     }
 
     private void FindNewDest(Vector3 destArrived)
     {
-        OffMeshLink[] links = FindObjectsOfType<OffMeshLink>();        
-        List<Vector3> destinations = new List<Vector3>();
+        List<Vector3> possibleDests = new List<Vector3>();
 
-        foreach (Transform tr in destList)
+        foreach (Vector3 destPos in destList)
         {
-            bool isDestArrived = (tr.position.x == destArrived.x && tr.position.z == destArrived.z);
+            bool isDestArrived = (destPos.x == destArrived.x && destPos.z == destArrived.z);
             if (!isDestArrived)
-                destinations.Add(tr.position);
+                possibleDests.Add(destPos);
         }
-
-        foreach (OffMeshLink link in links)
-        {            
-            if (link.transform.position != destArrived)            
-                destinations.Add(link.transform.position);       
-        }    
 
         int closestDestIndex = 0;
         float minDist = Mathf.Infinity;
 
-        for(int i = 0; i < destinations.Count; i++)
+        for (int i = 0; i < possibleDests.Count; i++)
         {
-            float dist = Vector3.Distance(transform.position, destinations[i]);
+            float dist = Vector3.Distance(transform.position, possibleDests[i]);
             if (dist < minDist)
             {
                 minDist = dist;
                 closestDestIndex = i;
             }
-        }        
-        agent.destination = destinations[closestDestIndex];
+        }
+        agent.destination = possibleDests[closestDestIndex];
     }
 
 
     void Update()
     {
-        /*if (agent.isOnOffMeshLink)
-            Debug.Log("on off mesh link");
-        else
-            Debug.Log("no off mesh");*/
-
-
         if (!switchingDest && agent.remainingDistance <= 0.01f)
-        {                                    
-            //Debug.Log("got to dest, find new dest");            
+        {
+            Debug.Log("got to dest, find new dest");
             switchingDest = true;
             FindNewDest(agent.destination);
             return;
-        }            
-        else if(switchingDest)
+        }
+        else if (switchingDest)
         {
             destCooldown += Time.deltaTime;
-            if(destCooldown >= maxDestCooldown)
+            if (destCooldown >= maxDestCooldown)
             {
                 switchingDest = false;
                 destCooldown = 0f;
@@ -144,12 +123,13 @@ public class Enemy : Character
             playerNear = true;
         else
             playerNear = false;
-        
+
         playerSighted = PlayerSighted(transform.position);
 
-        if(playerNear && playerSighted)
-        {            
-            agent.destination = player.transform.position;                 
+        if (playerNear && playerSighted)
+        {
+            Debug.Log("going to player");
+            agent.destination = player.transform.position;
 
             if (shootCooldown >= maxShootCooldown)
             {
@@ -157,12 +137,12 @@ public class Enemy : Character
                 shootCooldown = 0f;
             }
         }
-        else if(agent.destination == player.transform.position)
+        else if (agent.destination == player.transform.position)
         {
-            //Debug.Log("Find dest other than player");
+            Debug.Log("Find dest other than player");
             FindNewDest(agent.destination);
         }
 
-        shootCooldown += Time.deltaTime;                
+        shootCooldown += Time.deltaTime;
     }
 }
