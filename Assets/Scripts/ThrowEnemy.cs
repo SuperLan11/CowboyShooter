15e44cdbc1c7d83e39b3dc66c8e4577fe88b76e6
SuperLan11 <92647630+SuperLan11@users.noreply.github.com>
2024-11-19 23:36:34 -0500

    got more advanced enemy throw code working

--- Assets/Scripts/ThrowEnemy.cs
+++ Assets/Scripts/ThrowEnemy.cs
@@ -14,6 +14,8 @@ public class ThrowEnemy : Enemy
 
     [SerializeField] public AudioSource boomSfx;
 
+    [SerializeField] private float wallHeightInc;
+
     private void ThrowTNT()
     {
         // tntChild is not a TNT prefab. 
@@ -25,50 +27,86 @@ public class ThrowEnemy : Enemy
         float distMult = distToPlayer / sightRange;
         
         GameObject newTnt = Instantiate(tntPrefab, tntSpawn.position, Quaternion.identity);
-        float yVel = VelToClearWall();
-        newTnt.GetComponent<Rigidbody>().velocity = new Vector3(0, yVel, 0);
+        //float yVel = VelToClearWall();
+        Vector3 tntVel = VelToClearWall();
+        newTnt.GetComponent<Rigidbody>().velocity = tntVel;
+        Debug.Log("newTnt vel is: " + newTnt.GetComponent<Rigidbody>().velocity);
         //newTnt.GetComponent<Rigidbody>().velocity = new Vector3(0, yThrowVel, 0);
 
-        Vector3 playerDirection = (player.transform.position - transform.position).normalized;
-        newTnt.GetComponent<Rigidbody>().velocity += playerDirection * maxThrowSpeed * distMult;
-
-        VelToClearWall();
+        //Vector3 playerDirection = (player.transform.position - transform.position).normalized;
+        //newTnt.GetComponent<Rigidbody>().velocity += playerDirection * maxThrowSpeed * distMult;        
     }
 
-    private float VelToClearWall()
+    private Vector3 VelToClearWall()
     {
         RaycastHit hit;
         //Vector3 direction = transform.forward;
-        Vector3 direction = (player.transform.position - transform.position).normalized;   
+        Vector3 playerDirection = (player.transform.position - transform.position).normalized;   
         
         Vector3 levelPlayerPos = player.transform.position;
         levelPlayerPos.y = transform.position.y;
         float distToLevelPlayer = Vector3.Distance(transform.position, levelPlayerPos);
 
-        float distToPlayer = Vector3.Distance(transform.position, player.transform.position);        
-        float angle = Mathf.Acos(distToLevelPlayer/distToPlayer);
-        angle = angle * Mathf.Rad2Deg;
-        Debug.Log("angle to player: " + angle);
-        //float angle = 0;
+        float distToPlayer = Vector3.Distance(transform.position, player.transform.position);                
+        bool isValidThrow = false;
+        bool hitWall = (Physics.Raycast(transform.position, playerDirection, out hit, sightRange));
 
-        while (Physics.Raycast(transform.position, direction, out hit, sightRange) && angle <= 88f)
-        {            
-            angle += (200f * Mathf.Deg2Rad);
-            float distToHit = Vector3.Distance(transform.position, hit.point);
+        // y0= 9.8*sqrt(wallHeight/4.9). initial y velocity needed to max y position at wallHeight
+        // landTime= 2*sqrt(wallHeight/4.9). time from throwing to hit player, assuming y diff is 0
+        // x0= horDist/landTime. initial x velocity needed to reach player by time
+        // check if y(dist/totalDist * landTime) >= wallTop
 
-            // sin(angle) = yDifference/distToHit
-            // yDifference = distToHit * sin(angle)
-            direction = hit.point - transform.position;
-            direction.y = distToHit * Mathf.Sin(angle*Mathf.Deg2Rad);
-            direction = direction.normalized;
+        float halfTntHeight = tntPrefab.GetComponent<CapsuleCollider>().height / 2;
+        Debug.Log("half tnt height: " + halfTntHeight);
+        float relativeWallHeight = hit.collider.bounds.max.y + halfTntHeight*2 - transform.position.y;
+        // increase heightDiff until valid throw or angle limit exceeded
+        float yV0 = 9.8f * Mathf.Sqrt(relativeWallHeight / 4.9f);
+        float landTime = 2f * Mathf.Sqrt(relativeWallHeight / 4.9f);
+        //float xV0 = distToLevelPlayer / landTime;
+        float xV0 = (distToLevelPlayer / landTime) * playerDirection.x;
+        float zV0 = (distToLevelPlayer / landTime) * playerDirection.z;
+        
+        float distToWall = Vector3.Distance(transform.position, hit.point);
+        float t = (distToWall / distToPlayer) * landTime;
+        // how to get distToEndOfWall?
 
-            GameObject hitObj = Instantiate(new GameObject(), hit.point, Quaternion.identity);
-            hitObj.name = "LastHit" + angle;
-            Debug.Log("last angle: " + angle);
+        bool aboveWallStart = (yV0 * t - 4.9f * t * t > relativeWallHeight);
+        //bool aboveWallEnd = (yV0 * )
+        // get angle to throw to just pass wall
+        float angle = Mathf.Atan(yV0 / xV0);
+        float adjustedWallHeight = relativeWallHeight;
+
+        while(!isValidThrow && angle <= 89)
+        {
+            angle += (100f * Mathf.Deg2Rad);
+
+            //relativeWallHeight = hit.collider.bounds.max.y + halfTntHeight - transform.position.y;
+            adjustedWallHeight += wallHeightInc;
+
+            // use sin and cos to get new yV0 and xV0? no, wallHeight
+            yV0 = 9.8f * Mathf.Sqrt(adjustedWallHeight / 4.9f);
+            landTime = 2f * Mathf.Sqrt(adjustedWallHeight / 4.9f);
+            //float xV0 = distToLevelPlayer / landTime;
+            //xV0 = (distToLevelPlayer / landTime) * playerDirection.x;
+            xV0 = (distToPlayer / landTime) * playerDirection.x;
+            //zV0 = (distToLevelPlayer / landTime) * playerDirection.z;
+            zV0 = (distToPlayer / landTime) * playerDirection.z;
+
+            Debug.Log("yV0: " + yV0);
+
+            distToWall = Vector3.Distance(transform.position, hit.point);
+            t = (distToWall / distToPlayer) * landTime;
+            // how to get distToEndOfWall?
+
+            aboveWallStart = (yV0 * t - 4.9f * t * t > relativeWallHeight);
+            if (aboveWallStart)
+            {
+                Debug.Log("angle " + angle + " was valid throw");
+                isValidThrow = true;
+            }
         }
-        //Debug.Log("last hit: " + hit.point);        
-        // figure out how to convert this to velocity later
-        return angle / 3.5f;
+
+        return new Vector3(xV0, yV0, zV0);                
     }
 
     // Update is called once per frame
