df4d25e966371c737df7a54635837262784399fb
Patrick-Mayer <53142589+Patrick-Mayer@users.noreply.github.com>
2024-12-04 16:26:52 -0500

    pause menu graphical issue fixed

--- Assets/Scripts/PlayerHUD.cs
+++ Assets/Scripts/PlayerHUD.cs
@@ -4,9 +4,12 @@ using UnityEngine;
 
 public class PlayerHUD : MonoBehaviour
 {
+    [SerializeField] private GameObject fadePanel;
+
     public void EnableHUD()
     {
         this.gameObject.SetActive(true);
+        fadePanel.SetActive(false);
     }
 
     public void DisableHUD()
